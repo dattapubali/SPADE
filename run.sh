73a38282feddf162f6118ba1b0667196e2a97c3a
Wajih Ul Hassan <whassan3@illinoi.edu>
2019-02-14 19:46:00 +0000

    Adding some stuff

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 ./bin/spade stop
 /bin/rm ./log/*
+ps -fA | grep -E 'SPADE'| awk '{print $2}'| while read line; do sudo kill -9 "$line"; done
 sudo /bin/rm -f /var/log/audit/audit.log
 sudo service auditd restart
 /bin/rm -f /tmp/provenance.dot
 /bin/rm -f /tmp/audit.log
 /bin/rm -rf /tmp/spade.graph_db
 ./bin/spade start
+sleep 5
+auditctl -l
+#sudo sh $1
